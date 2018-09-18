be2eaacea7a4386197b2cad19f757e5811325fbd
Laura Miller <lmm320@miami.edu>
2018-09-18 17:23:51 -0400

    readme

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,17 +1,7 @@
 function setup() {
   // put setup code here
-  createCanvas(400,400);
-  background(0);
-  background(255,204,02);
-  background("orange");
-  background("#333333");
-
-
 }
 
 function draw() {
   // put drawing code here
-  ellipse(120,120,50,50);
-  ellipse(220,120,50,50);
-  rect(95,190,150,20);
 }
