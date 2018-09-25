a10b68c2c6122120d65a7930e72f55f80046c980
Laura Miller <lmm320@miami.edu>
2018-09-25 09:23:08 -0400

    change

--- hw/character/sketch.js
+++ hw/character/sketch.js
@@ -1,67 +1,19 @@
-var a = 1;
-var num1 = 3;
-var b = 'b';
-var word1 = "word";
-var trueFalse = true;
-var sentences = "I'm a sentence";
+//head
+size(200, 200);
+fill(0, 255, 0); //green
+ellipse(100, 100, 150, 150);
 
-var charColor = "orange";
-var eyeSize = 20;
-
-var facePosX = 100;
-var facePosY = 100;
-
-function setup() {
-  // put setup code here
-  createCanvas(400,400);
-  background(0);
-  background(255,255,255);
-  background("orange");
-  background("#333333");
-
-  console.log(a + num1);
-  console.log(a-num1);
-  console.log(a + b);
-  //Single line comment
-  //console.log(a * b);
-
-  /*
-  Block of comments
-  */
-
-  //this is only in setup scope, only available in setup
-  var setupVariable = 5;
-}
-
-function draw() {
-  background("white");
-  facePosX = mouseX;
-  facePosY = mouseY;
-  // put drawing code here();
-  stroke('black');
-  strokeWeight(num1);
-//does not work, setupVariable's scope is only in the setup.
-//  strokeWeight(setupVariable);
-
-
-  fill(charColor);
 //eyes
-  ellipse(facePosX + 50,facePosY,eyeSize,eyeSize);
-  ellipse(facePosX + 110,facePosY,eyeSize,eyeSize);
-  //mouth
-  rect(facePosX,facePosY + 200,180,20);
-//mole
-  stroke('green');
-  strokeWeight(5);
-  point(width/2, height/2);
+fill(255); //white
+ellipse(75, 85, 30, 20);
+ellipse(125, 85, 30, 20);
+//pupils
+fill(0); //black
+ellipse(75, 85, 10, 10);
+ellipse(125, 85, 10, 10);
 
-  line(facePosX,facePosY + 210,facePosX + 180,facePosY + 210);
-//pacperson
-  arc(facePosX + 100,facePosY +100,100,100,QUARTER_PI, TWO_PI - QUARTER_PI);
+//mouth
 
-
-}
-
-function mousePressed(){
-  charColor = "blue";
-}
+fill(255, 0, 0); //red
+arc(100, 125, 80, 50, 0, 3.14);
+line(60, 125, 140, 125);
