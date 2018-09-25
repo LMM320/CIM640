b8baa98bdd91111f699889c0c2c4525dd92ee21c
Laura Miller <lmm320@miami.edu>
2018-09-25 14:22:04 -0400

    changes

--- hw/character/sketch.js
+++ hw/character/sketch.js
@@ -1,19 +1,17 @@
-//head
-size(200, 200);
-fill(0, 255, 0); //green
-ellipse(100, 100, 150, 150);
 
-//eyes
-fill(255); //white
-ellipse(75, 85, 30, 20);
-ellipse(125, 85, 30, 20);
-//pupils
-fill(0); //black
-ellipse(75, 85, 10, 10);
-ellipse(125, 85, 10, 10);
+var x = 400;
+var y = 400;
 
-//mouth
+function setup() {
+  // put setup code here
+  createCanvas(x,y);
+  background("#58FAF4");
+}
 
-fill(255, 0, 0); //red
-arc(100, 125, 80, 50, 0, 3.14);
-line(60, 125, 140, 125);
+function draw() {
+  background("#58FAF4");
+}
+
+
+//end of code
+}
