04957b8ac0bfd2decd532d5ab98be316eab7fdaf
Laura Miller <lmm320@miami.edu>
2018-10-09 17:08:00 -0400

    commit

--- classexamples/frameAnimationStart/sketch.js
+++ classexamples/frameAnimationStart/sketch.js
@@ -1,13 +1,17 @@
 var frameAmounts = 4;
 var frameArray = [];
+var currentFrame = 0;
+
+var interval = 2000;
+var prevMillis = 0;
 
 function preload(){
   // var frameString = "assets/Thatwasclose0.jpg";
   // frameArray[0] = loadImage(frameString);
-
-  for(var frames = 0); frames< frameAmounts; frames++){
+//notes: starting point, condition, and how it iterates
+  for(var frames = 0; frames< frameAmounts; frames++){
     var frameString = "assets/Thatwasclose" + frames + ".jpg";
-    frameArray[0] = loadImage(frameString);
+    frameArray[frames] = loadImage(frameString);
   }
 }
 
@@ -16,5 +20,24 @@ function setup(){
 }
 
 function draw(){
-  image(frameArray[0],0,0);
+  //frameRate(1)
+  //console.log(millis());
+  image(frameArray[currentFrame],0,0);
+
+if(millis() - prevMillis> interval){
+  currentFrame++;
+  prevMillis = millis();
 }
+  //console.log(currentFrame);
+  if(currentFrame > frameAmounts-1){
+    currentFrame = 0;
+  }
+
+ellipse(mouseX,mouseY,10,10);
+
+}
+
+
+
+
+////end of code
